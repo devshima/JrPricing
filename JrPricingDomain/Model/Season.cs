44f0edf15bc7cbb4defccf8a4b88e76418a902d8
YASUHISA <YASUHISA@IP-C613B678>
2020-01-29 05:11:07 +0900

    refactor delete comment

--- JrPricingDomain/Model/Season.cs
+++ JrPricingDomain/Model/Season.cs
@@ -6,18 +6,6 @@ namespace JrPricingDomain.Model
 {
     public interface Season
     {
-        //public static Season offPeak = new Season(new ReservedSeatSeasonVariable(-200));
-        //public static Season peak = new Season(new ReservedSeatSeasonVariable(200));
-        //public static Season regular = new Season(new ReservedSeatSeasonVariable(0));
-
-        //private readonly ReservedSeatSeasonVariable reservedSeatSeasonVariable;
-
-        //public Season(ReservedSeatSeasonVariable reservedSeatSeasonVariable)
-        //{
-        //    this.reservedSeatSeasonVariable = reservedSeatSeasonVariable;
-        //}
-
         int variableAmount();
-
     }
 }
